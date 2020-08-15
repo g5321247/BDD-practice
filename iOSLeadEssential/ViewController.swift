b4225689cce8e364ad291c8f9faf43039a136a81
GeorgeLIu <g5321247@gmail.com>
2020-08-15 14:26:25 +0800

    initial commit

---
+++ iOSLeadEssential/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSLeadEssential
+//
+//  Created by George Liu on 2020/8/15.
+//  Copyright © 2020 George Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
