b0969ee47dfd016361d1e35cfe94b73b2a2cb109
GeorgeLIu <g5321247@gmail.com>
2020-08-18 11:31:58 +0800

    [Add] create `FeedItem` Model

---
+++ iOSLeadEssential/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  iOSLeadEssential
+//
+//  Created by George Liu on 2020/8/18.
+//  Copyright © 2020 George Liu. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
