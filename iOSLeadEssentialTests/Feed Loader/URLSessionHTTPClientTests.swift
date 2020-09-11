4e8b4873d20c6d358eb2dffc8ddb6c594a50dc9e
GeorgeLIu <g5321247@gmail.com>
2020-09-11 21:39:10 +0800

    [Test] Add end-to-end API test in a separate test target

--- iOSLeadEssentialTests/Feed Loader/URLSessionHTTPClientTests.swift
+++ iOSLeadEssentialTests/Feed Loader/URLSessionHTTPClientTests.swift
@@ -9,30 +9,6 @@
 import XCTest
 @testable import iOSLeadEssential
 
-// 在測試的保護下，即使物件改成 URLSession 的 extension，跑測試還是沒問題
-class URLSessionHTTPClient: HTTPClient {
-
-    let session: URLSession
-
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-
-    struct UnexpectedValuesRepresentation: Error {}
-
-    func get(from url: URL, completion: @escaping (HTTPResult) -> Void) {
-        session.dataTask(with: url, completionHandler: { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success((data, response)))
-            } else {
-                completion(.failure(UnexpectedValuesRepresentation()))
-            }
-        }).resume()
-    }
-}
-
 class URLSessionHTTPClientTests: XCTestCase {
 
     override func setUp() {
