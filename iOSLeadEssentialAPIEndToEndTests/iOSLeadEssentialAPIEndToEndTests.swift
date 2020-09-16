ee372ab12400c9d0e972d288825e61dff8f0fe1a
GeorgeLIu <g5321247@gmail.com>
2020-09-17 07:59:36 +0800

    Use ephemeral URL session configuration to avoid sharing state across test execution

--- iOSLeadEssentialAPIEndToEndTests/iOSLeadEssentialAPIEndToEndTests.swift
+++ iOSLeadEssentialAPIEndToEndTests/iOSLeadEssentialAPIEndToEndTests.swift
@@ -33,7 +33,7 @@ class iOSLeadEssentialAPIEndToEndTests: XCTestCase {
 
     private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> RemoteFeedLoader.Result? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedLoader(client: client, url: testServerURL)
 
         trackForMemoryLeak(client, file: file, line: line)
